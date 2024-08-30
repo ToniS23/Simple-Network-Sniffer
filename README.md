c00bbd0784008d28af63b4d1a37dd7169743a636
ToniS23 <asips2399@gmail.com>
2024-08-30 18:37:47 +0300

    Update README.md Add comments Print text that show that packets are being captured

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 ---
 # Simple-Network-Sniffer
 ---
-A simple weekend project in Python to learn sockets, network programming and scapy.
-
+Description: A simple weekend project in Python to learn sockets, network programming and scapy.
+---
 Main functionalities:
 - sniff network traffic
 - filter packets
 - craft packets
 - UI implementation
+---
--- main.py
+++ main.py
@@ -12,26 +12,28 @@ main.title("Simple Packet Sniffer")
 main.geometry("800x700")
 main.resizable(False, False)
 
-output_text = scrolledtext.ScrolledText(main, width=80, height=30, wrap=tk.WORD)
+output_text = scrolledtext.ScrolledText(main, width=80, height=30, wrap=tk.WORD) # main widget
 output_text.pack(padx=10, pady=10)
 
 def capture_packets():
     old_stdout = sys.stdout
     sys.stdout = captured_output = io.StringIO()
     try:
-        packets = sniff(count=10)
-        packets.show()
-        output_text.insert(tk.END, captured_output.getvalue())
+        packets = sniff(count=10) # this will block the event loop because it doesn't return control only after it captures packets
+        packets.show() # this is NOT returned as a string so it can't be output to the widget
+        output_text.insert(tk.END, captured_output.getvalue()) # send the value of the StringIO object to the widget
         output_text.see(tk.END)
     finally:
-        sys.stdout = old_stdout
+        sys.stdout = old_stdout # restore old stdoutput
 
+# we need this function to not block the main thread on which our UI runs (event loop)
 def start_capture():
+    output_text.insert(tk.END, "[+]-Capturing packets, please wait....\n")
     capture_thread = threading.Thread(target=capture_packets)
-    capture_thread.start()
+    capture_thread.start() # start the separate thread for capturing packets
     
 
 update_button = tk.Button(main, text='Capture 10 packets :)', command=start_capture)
-update_button.pack(pady=10)
+update_button.pack(pady=20)
 
 main.mainloop()
